016cb26f19d47eea5934c94ef8c23811eb6df1e2
kbbs1 <ksua211@mywhitecliffe.com>
2023-06-30 21:24:02 +1200

    Added docstrings
    Added docstrings for the functions in the BowlingGame file.

--- BowlingGame.py
+++ BowlingGame.py
@@ -8,6 +8,15 @@ class BowlingGame:
         self.rolls.append(pins)
 
     def score(self):
+        """
+        Score calculation based on the number of pins bowled.
+
+        Args:
+            rollIndex (int): number of the frame being bowled.
+
+        Returns:
+            Result of total calculation of pins bowled depending on strike, spare or random.      
+        """
         result = 0
         rollIndex=0
         for frameIndex in range(10):
@@ -23,14 +32,61 @@ class BowlingGame:
         return result
 
     def isStrike(self, rollIndex):
+        """
+        Statement if results is a strike, ten pins within one frame, one bowl.
+
+        Args:
+            rollIndex (int): number of the frame being bowled.
+
+        Returns:
+            Result of strike which is 10 points      
+        """
         return self.rolls[rollIndex] == 10
+    
     def isSpare(self, rollIndex):
+        """
+        Statement if results is a spare, ten pins within one frame, two bowls.
+
+        Args:
+            rollIndex (int): number of the frame being bowled.
+
+        Returns:
+            Result of spare which is 10 points      
+        """
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
+    
     def strikeScore(self,rollIndex):
+        """
+        Provides formula if results is a strike.
+
+        Args:
+            rollIndex (int): number of the frame being bowled.
+
+        Returns:
+            Result of strike 10 points added with the points from the next two frames.      
+        """
         return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
 
     def spareScore(self,rollIndex):
+        """
+        Provides formula if results is a spare.
+
+        Args:
+            rollIndex (int): number of the frame being bowled.
+
+        Returns:
+            Result of spare 10 points added with the points from the next frame.      
+        """
         return  10+ self.rolls[rollIndex+2]
 
     def frameScore(self, rollIndex):
+        """
+        Provides formula if results is a random ie not strike or spare.
+
+        Args:
+            rollIndex (int): number of the frame being bowled.
+
+        Returns:
+            Result of points on current frame and next frame.      
+        """
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
