1c9cff8bb07101a5a4efa5a8f3f1fad3f22f3554
kbbs1 <ksua211@mywhitecliffe.com>
2023-06-30 21:40:27 +1200

    Added docstrings
    Added docstrings to methods on the Test file to describe each unit test

--- Test.py
+++ Test.py
@@ -4,21 +4,76 @@ import BowlingGame
 class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
+        #Initialize Bowling Game        
         self.game = BowlingGame.BowlingGame()
 
+    def rollMany(self, pins,rolls):
+        """
+        Method to specify number of pins bowled and number of rolls by bowler.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.   
+        """
+        for i in range(rolls):
+            self.game.roll(pins)
+
     def testGutterGame(self):
+        """
+        Method to test if score calculated matches expected score, for when no pins bowled at all.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.
+
+        Returns:
+            Score calculated using method in Bowling Game file.      
+        """
         self.rollMany(0, 20)
         self.assertEqual (0,self.game.score())
+
     def testAllOnes(self):
+        """
+        Method to test if score calculated matches expected score, for when one pin bowled in the specified number of rolls.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.
+            
+        Returns:
+            Score calculated using method in Bowling Game file.      
+        """
         self.rollMany(1, 20)
         self.assertEqual(20, self.game.score())
+
     def testOneSpare(self):
+        """
+        Method to test if score calculated matches expected score, for when there is only one spare.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.
+            
+        Returns:
+            Score calculated using method in Bowling Game file.      
+        """
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
         self.rollMany(0,17)
         self.assertEqual(16,self.game.score())
+
     def testOneStrike(self):
+        """
+        Method to test if score calculated matches expected score, for when there is only one strike.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.
+            
+        Returns:
+            Score calculated using method in Bowling Game file.      
+        """
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
@@ -26,10 +81,30 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(24, self.game.score())
 
     def testPerfectGame(self):
+        """
+        Method to test if score calculated matches expected score, for when strike is scored for all 10+2 bonus frames.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.
+            
+        Returns:
+            Score calculated using method in Bowling Game file.      
+        """
         self.rollMany(10,12)
         self.assertEqual(300, self.game.score())
 
     def testComboGame(self):
+        """
+        Method to test if score calculated matches expected score, for when there is a strike, spare, and random numbers of pin scores.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.
+            
+        Returns:
+            Score calculated using method in Bowling Game file.      
+        """
         self.game.roll(10)
         self.game.roll(8)
         self.game.roll(2)
@@ -41,6 +116,16 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(44 , self.game.score())
 
     def testTwoStrikeScores(self):
+        """
+        Method to test if score calculated matches expected score, for when there are two strikes.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.
+            
+        Returns:
+            Score calculated using method in Bowling Game file.      
+        """
         self.game.roll(10)
         self.game.roll(10)
         self.game.roll(4)
@@ -49,6 +134,16 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(46, self.game.score())
     
     def testTwoSpareScores(self):
+        """
+        Method to test if score calculated matches expected score, for when there are two spares.
+
+        Args:
+            pins (int): number of pins bowled.
+            rolls (int): number of rolls by bowler.
+            
+        Returns:
+            Score calculated using method in Bowling Game file.      
+        """
         self.game.roll(3)
         self.game.roll(5)
         self.game.roll(6)
@@ -59,6 +154,6 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(36, self.game.score())
     
 
-    def rollMany(self, pins,rolls):
-        for i in range(rolls):
-            self.game.roll(pins)
+    # def rollMany(self, pins,rolls):
+    #     for i in range(rolls):
+    #         self.game.roll(pins)
